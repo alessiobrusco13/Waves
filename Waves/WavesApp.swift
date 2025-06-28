a8152c1a0a1fceea5b05d536d2316f3eb27e1fa2
Alessio Garzia Marotta Brusco <alessiobrusco609@gmail.com>
2025-06-28 17:04:51 +0200

    Added files.

---
+++ Waves/WavesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WavesApp.swift
+//  Waves
+//
+//  Created by Alessio Garzia Marotta Brusco on 28/06/25.
+//
+
+import SwiftUI
+
+@main
+struct WavesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
